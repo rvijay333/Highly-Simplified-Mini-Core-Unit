a4314c4ea9a5207398c508387cc2a9b14d81ab9b
rvijay333 <rvj151003@gmail.com>
2025-06-08 19:20:27 +0530

    update1_alu.v

--- update_1/code/alu.v
+++ update_1/code/alu.v
@@ -1,1 +1,20 @@
+module update1_alu (
+    input [2:0] opcode,
+    input [15:0] a, b,
+    output reg [15:0] result
+);
+    always @(*) begin
+        case (opcode)
+            3'b000: result = a + b;
+            3'b001: result = a - b;
+            3'b010: result = a & b;
+            3'b011: result = a | b;
+            3'b100: result = a ^ b;
+            3'b101: result = a * b;
+            3'b110: result = a + 1;
+            3'b111: result = a - 1;
+            default: result = 0;
+        endcase
+    end
+endmodule
 
